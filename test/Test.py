93c1a3c382c90ab2e799f11fc5d058c77e07fe16
Christian <chris3151@ciencias.unam.mx>
2018-09-23 18:25:15 -0500

    Las pruebas ya funcionan al 100%

--- test/Test.py
+++ test/Test.py
@@ -12,50 +12,50 @@ import unittest
 import socket
 import threading
 import sys
+import time
 #----Clase de pruebas unitarias
 class TestCliente(unittest.TestCase):
     #----Método que acepta clientes
     def runnable(self):
         self.conexion, ip = self.sock.accept()
     #----Método que monta un pequeño servidor
-    def setUp(self):
+    def set_up(self):
         self.sock = socket.socket()
-        self.sock.bind(("0.0.0.0",8000))
+        self.sock.bind(("0.0.0.0",0))
         self.sock.listen(0)
         threading.Thread(target = self.runnable).start()
     #----Test del método conecta
     def test_conecta(self):
         cliente = Cliente()
         cliente.crea_socket()
-        cliente.conecta(("localhost",8000))
+        self.set_up()
+        cliente.conecta(("localhost", self.sock.getsockname()[1]))
         self.assertTrue(cliente.is_conectado())
         self.desconecta()
     #----Método que mata al servidor
     def desconecta(self):
         self.sock.close()
-    #----Test de la comunicación entre el cliente y el servidor
-    def test_recibe(self):
-        cliente = Cliente()
-        cliente.crea_socket()
-        cliente.conecta(("localhost",8000))
-        with self.assertRaises(ValueError):
-            self.assertEquals("Hola",cliente.recibe_mensaje())
-        self.desconecta()
     #----Test que desconecta al cliente
     def test_desconecta(self):
+        time.sleep(0.0005)
         cliente = Cliente()
         cliente.crea_socket()
-        cliente.conecta(("localhost", 8000))
+        self.set_up()
+        cliente.conecta(("localhost", self.sock.getsockname()[1]))
         with self.assertRaises(SystemExit):
             cliente.maneja_evento("DISCONNECT"+"\n")
         self.desconecta()
     #----Test de enviar y manejar los eventos
-    def test_envia(self):
+    def test_envia_recibe(self):
+        time.sleep(0.0005)
         cliente = Cliente()
         cliente.crea_socket()
-        cliente.conecta(("localhost",8000))
+        self.set_up()
+        cliente.conecta(("localhost",self.sock.getsockname()[1]))
         cliente.maneja_evento("IDENTIFY yo" + "\n")
         self.assertEquals("IDENTIFY yo", self.conexion.recv(1024).encode('UTF-8'))
+        self.conexion.send("Hola".encode('UTF-8'))
+        self.assertEquals(cliente.recibe_mensaje(),"Hola")
         self.desconecta()
         
 
